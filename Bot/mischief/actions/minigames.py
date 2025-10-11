af9aed714d0a1a6a4693773edc0bdf93675bcd73
11-pog <alvessamuel872@gmail.com>
2025-10-11 16:37:06 -0300

    Fix check method to correctly identify 'minigame' in normalized text

--- Bot/mischief/actions/minigames.py
+++ Bot/mischief/actions/minigames.py
@@ -6,7 +6,7 @@ class MINIGAMES(TextMischief):
     mischief_description = "reacts with pandemonium emote every time there is 'minigame' in a message"
     
     def check(self, normalized_text, message):
-        return 'minigames' in normalized_text
+        return 'minigame' in normalized_text
     
     async def execute(self, normalized_text, message):
         await message.add_reaction("<:MINIGAMES:1400192834073530400>")
