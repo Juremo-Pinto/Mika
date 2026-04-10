0e0bda9497684835349dba094fcb9894c3905f1e
Jelado1234 <fredericoteodoroarantes@gmail.com>
2026-04-09 22:47:11 -0300

    Fat fuck caused problems so i fixed it :D

--- bot/mischief/actions/fat_fuck.py
+++ bot/mischief/actions/fat_fuck.py
@@ -6,7 +6,7 @@ class FatFuck(TextMischief):
     mischief_description = "Fat Fuck"
     
     def check(self, normalized_text, message):
-        return normalized_text == 'fat fuck' and message.author.id != self._bot.user.id and message.author.id !=
+        return normalized_text == 'fat fuck' and message.author.id != self._bot.user.id
     
     async def execute(self, normalized_text, message):
         await message.channel.send("LMAO")
